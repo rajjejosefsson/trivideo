fcafa00cd872a19a1741bf9a611331050960973a
Rasmus Josefsson <josefssonrasmus@hotmail.se>
2018-04-12 12:01:58 +0200

    d

--- src/views/startpage.js
+++ src/views/startpage.js
@@ -23,7 +23,7 @@ class StartPage extends Component {
             onChange={e => this.setState({ roomName: e.target.value })}
           />
           <button className="startpage__go-btn" onClick={this.onSubmit}>
-            Go
+            Goo
           </button>
         </div>
       </div>
